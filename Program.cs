a14c5580e62e83f5ff1b66eeacacc3213d5065b0
rekabalogh <baloghreka16@gmail.com>
2025-05-20 21:02:46 +0200

    teszt

--- Program.cs
+++ Program.cs
@@ -3,13 +3,14 @@ using System.Windows.Forms;
 
 namespace Rendeleskezelo
 {
+//asda
     internal static class Program
     {
 
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
+            Application.EnableVisualStyles(); //asd
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
         }
